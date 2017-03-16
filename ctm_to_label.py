6dd6cd8e5bea2bb6f864120496ffa3ae3a436207
Ryan Shean <ryanshean1@gmail.com>
2017-03-16 16:07:34 -0700

    Added Description

--- ctm_to_label.py
+++ ctm_to_label.py
@@ -1,5 +1,5 @@
 __author__ = 'Ryan Shean'
-
+# This is like some gargabe script that I used for sphinx maybe? Probobly won't need to use unlesss you're outputting CTM files and need to extract them from audio using audacity labels
 
 int2words = open('words.txt')
 dictionary = []
